b4db0ae29ccdf78dabcdb7cbea7cee62ac4bb98c
burcu <bozcan@gmail.com>
2020-01-13 14:15:36 -0800

    Adding pop up dialog box when user clicks experiences

---
+++ MrsAgileWebSite/wwwroot/js/about.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  //  $("#aboutDetailSectionModal").modal('show');
+});
+
+
+var openAboutDialog = function () {
+    $("#aboutDetailSectionModal").modal('show');
+};
+
+var closeAboutDialog = function () {
+    $("#aboutDetailSectionModal").modal('hide');
+};
